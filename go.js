0327025253ab1f0d9618f78c190dd968f94b2daf
gmstoreassistance-arch <gmstoreassistance@gmail.com>
2026-06-12 23:51:16 +0300

    f
    us

---
+++ go.js
@@ -0,0 +1,29 @@
+{
+  "type": "TRC20",
+  "standard": "TRC20",
+  "chain": "tron-nile",
+  "network": "nile",
+  "address": "TLhjWZ7ibnosj5B2bHvD1a2oecWpViNyvS",
+  "implementation": "TLhjWZ7ibnosj5B2bHvD1a2oecWpViNyvS",
+  "name": "Tether USDT",
+  "symbol": "USDT",
+  "decimals": 6,
+  "totalSupply": "1000000000000",
+  "totalSupplyUnits": "1000000000000000000",
+  "description": "Tether USDT TRC20 test token deployed on TRON Nile testnet.",
+  "logoCID": "QmTNerk9uE6Y46WH6yHfMZGKTnSBNxCrat1KHJEthrbnaN",
+  "logoURI": "ipfs://QmTNerk9uE6Y46WH6yHfMZGKTnSBNxCrat1KHJEthrbnaN",
+  "logo": "ipfs://QmTNerk9uE6Y46WH6yHfMZGKTnSBNxCrat1KHJEthrbnaN",
+  "logoGatewayURL": "https://ipfs.io/ipfs/QmTNerk9uE6Y46WH6yHfMZGKTnSBNxCrat1KHJEthrbnaN",
+  "localLogoGatewayURL": "http://127.0.0.1:8080/ipfs/QmTNerk9uE6Y46WH6yHfMZGKTnSBNxCrat1KHJEthrbnaN",
+  "logoLocalPath": "assets/logos/usdt.svg",
+  "explorer": "https://nile.tronscan.org/#/token20/TLhjWZ7ibnosj5B2bHvD1a2oecWpViNyvS",
+  "contractExplorer": "https://nile.tronscan.org/#/contract/TLhjWZ7ibnosj5B2bHvD1a2oecWpViNyvS",
+  "tags": [
+    "trc20",
+    "usdt",
+    "tether",
+    "nile-testnet"
+  ],
+  "updatedAt": "2026-06-12T23:45:00.000Z"
+}
